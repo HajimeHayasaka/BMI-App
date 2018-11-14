56b136a5c18797a4faf52a92056d9e828cdb8b48
HajimeHayasaka <lunasa-inion-2.scannlain@i.softbank.jp>
2018-11-14 23:02:14 +0900

    新規登録

---
+++ BMIApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMIApp
+//
+//  Created by 早坂甫 on 2018/11/14.
+//  Copyright © 2018年 早坂甫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
