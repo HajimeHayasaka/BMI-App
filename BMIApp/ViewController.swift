e2ac796bd22772ece891d40410df501f9fff62b3
HajimeHayasaka <lunasa-inion-2.scannlain@i.softbank.jp>
2018-11-17 11:36:47 +0900

    ボタンの色、形、変更。 ラベル追加。

--- BMIApp/ViewController.swift
+++ BMIApp/ViewController.swift
@@ -14,9 +14,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let button = UIButton()
-        button.backgroundColor = UIColor.orange
-        button.frame = CGRect(x: 40, y: 60, width: 100, height: 40)
+        button.backgroundColor = UIColor(red: 1.0, green: 0.7, blue: 0.7, alpha:1.0)
+        button.frame = CGRect(x: 100, y: 350, width: 100, height: 40)
+        button.setTitle("はじめる", for: .normal)
+        button.setTitleColor(UIColor.gray, for: .normal)
+        button.layer.cornerRadius = 10
         self.view.addSubview(button)
+        
+        let titleLabel = UILabel()
+        titleLabel.textColor = UIColor.gray
+        titleLabel.text = "BMI計算機"
+        titleLabel.frame = CGRect(x: 100, y: 250, width: 100, height: 40)
+        self.view.addSubview(titleLabel)
+        
+        
+//        button.addTarget(<#T##target: Any?##Any?#>, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
