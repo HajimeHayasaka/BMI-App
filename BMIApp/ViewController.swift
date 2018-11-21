ada94a860136964cd90180eed5fd11f3f2f987bd
HajimeHayasaka <lunasa-inion-2.scannlain@i.softbank.jp>
2018-11-21 23:37:06 +0900

    - ボタンとテキストのカラーを定義。buttonPink, textGray。 - ViewControllerのカラー設定処理を修正。 - InputViewController.swiftを新規作成。 - ViewControllerにボタン押下時の処理追加。InputViewControllerへの画面遷移。

--- BMIApp/ViewController.swift
+++ BMIApp/ViewController.swift
@@ -13,23 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.view.backgroundColor = UIColor.white
+        
         let button = UIButton()
-        button.backgroundColor = UIColor(red: 1.0, green: 0.7, blue: 0.7, alpha:1.0)
-        button.frame = CGRect(x: 100, y: 350, width: 100, height: 40)
+        button.backgroundColor = UIColor(named: "buttonPink")
+        button.frame = CGRect(x: view.frame.width * 0.1, y: 350, width: view.frame.width * 0.8, height: 40)
         button.setTitle("はじめる", for: .normal)
-        button.setTitleColor(UIColor.gray, for: .normal)
+        button.setTitleColor(UIColor(named: "textGray"), for: .normal)
         button.layer.cornerRadius = 10
         self.view.addSubview(button)
         
         let titleLabel = UILabel()
-        titleLabel.textColor = UIColor.gray
+        titleLabel.textColor = UIColor(named: "textGray")
         titleLabel.text = "BMI計算機"
         titleLabel.frame = CGRect(x: 100, y: 250, width: 100, height: 40)
         self.view.addSubview(titleLabel)
         
         
-//        button.addTarget(<#T##target: Any?##Any?#>, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+        button.addTarget(self, action: #selector(basicButtonClicked(sender:)), for:.touchDragExit)
+        // 1：何処の？（ViewController）　２：実行する関数　３：実行タイミング（ボタンを離した時に実行）
+        // #selectorは決まった文で必ず必要
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //memo:navigationControllerを隠す。
+        
+    }
+    @objc func basicButtonClicked(sender: UIButton){
+        print("basicButtonBtnClicked")
+        let secondVC:InputViewController = InputViewController()
+        self.navigationController?.pushViewController(secondVC, animated: true)
     }
 
 
