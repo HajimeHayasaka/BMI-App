c2b73a1bb80e69faf0a1db63a81f836fca00b066
HajimeHayasaka <lunasa-inion-2.scannlain@i.softbank.jp>
2018-11-25 19:44:30 +0900

    - 体重、身長入力の文を追加（入力欄は未作成） - BMI計算ボタンの追加（押下時の処理及び遷移先は未作成）

--- BMIApp/InputViewController.swift
+++ BMIApp/InputViewController.swift
@@ -14,11 +14,52 @@ class InputViewController: UIViewController {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.white
+        
+        
+        // 体重入力欄の作成
+        let weightLabel = UILabel()
+        weightLabel.textColor = UIColor(named: "textGray") // ボタンの色をグレー色（textGray）に設定
+        weightLabel.font = UIFont.systemFont(ofSize: 20) // フォントサイズを変更
+        weightLabel.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.4,
+                                  width: view.frame.width * 0.8, height: view.frame.height * 0.1) // 表示位置を設定
+        weightLabel.textAlignment = NSTextAlignment.left // 左寄せ
+        weightLabel.text = "体重を入力" // タイトルを設定
+        self.view.addSubview(weightLabel)
+
+        // 身長入力欄の作成
+        let heightLabel = UILabel()
+        heightLabel.textColor = UIColor(named: "textGray") // ボタンの色をグレー色（textGray）に設定
+        heightLabel.font = UIFont.systemFont(ofSize: 20) // フォントサイズを変更
+        heightLabel.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.6,
+                                   width: view.frame.width * 0.8, height: view.frame.height * 0.1) // 表示位置を設定
+        heightLabel.textAlignment = NSTextAlignment.left // 左寄せ
+        heightLabel.text = "身長を入力" // タイトルを設定
+        self.view.addSubview(heightLabel)
+
+        // BMI計算ボタンの生成
+        let bmiCalcButton = UIButton()
+        bmiCalcButton.backgroundColor = UIColor(named: "buttonPink")   // ボタンの色をピンク色（buttonPink）に設定
+        bmiCalcButton.frame = CGRect(x: view.frame.width * 0.25, y: view.frame.height * 0.8,
+                              width: view.frame.width * 0.5, height: view.frame.height * 0.1) // 横の位置を中央に設定
+        bmiCalcButton.setTitleColor(UIColor(named: "textGray"), for: .normal) // ボタンの文字をグレー色（textGray）に設定
+        bmiCalcButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        bmiCalcButton.layer.cornerRadius = 20 // ボタンに丸みを付ける
+        bmiCalcButton.setTitle("計算", for: .normal)
+        self.view.addSubview(bmiCalcButton)
+        
+        // BMI計算ボタンが押された時の処理を実行
+        bmiCalcButton.addTarget(self, action: #selector(bmiCalcButtonClicked(sender:)), for:.touchUpInside)
 
         // Do any additional setup after loading the view.
     }
     
-
+    // BMI計算ボタンを押された時の処理
+    @objc func bmiCalcButtonClicked(sender: UIButton){
+        print("bmiCalcButtonClicked")
+        let secondVC:InputViewController = InputViewController()
+        self.navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
