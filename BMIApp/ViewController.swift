91cddb5afc45f64ffabfcdf6657b602b168b1653
HajimeHayasaka <lunasa-inion-2.scannlain@i.softbank.jp>
2018-11-28 23:31:49 +0900

    - センター揃えテスト

--- BMIApp/ViewController.swift
+++ BMIApp/ViewController.swift
@@ -19,10 +19,12 @@ class ViewController: UIViewController {
         // タイトルの生成
         let titleLabel = UILabel()
         titleLabel.textColor = UIColor(named: "textGray") // ボタンの色をグレー色（textGray）に設定
-        titleLabel.font = UIFont.systemFont(ofSize: 60) // フォントサイズを変更
+        titleLabel.font = UIFont.systemFont(ofSize: 10) // フォントサイズを変更
         titleLabel.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.4,
                                   width: view.frame.width * 0.8, height: view.frame.height * 0.1) // 表示位置を設定
         titleLabel.textAlignment = NSTextAlignment.center // センター揃え
+        // UILabelの縦はデフォルト中央に設置される。
+//        titleLabel.backgroundColor = UIColor.red
         titleLabel.text = "BMI計算機" // タイトルを設定
         self.view.addSubview(titleLabel)
         
