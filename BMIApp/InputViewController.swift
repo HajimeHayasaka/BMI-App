922866e3c037b2956e8890f3bf8af1412070f0ee
HajimeHayasaka <lunasa-inion-2.scannlain@i.softbank.jp>
2018-12-08 15:47:26 +0900

    - 入力欄の位置を上部に修正（キーボードと重ならないように） - 下線を作成 - returnKey押下時の処理追加 - クリアボタンの追加 - 入力欄に単位を表示 - 入力値をInt型で保持するためのテスト追加

--- BMIApp/InputViewController.swift
+++ BMIApp/InputViewController.swift
@@ -11,62 +11,107 @@ import UIKit
 class InputViewController: UIViewController, UITextFieldDelegate {
 
     let weightInputField = UITextField()
+    let heightInputField = UITextField()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // 背景色を変更（白）
         self.view.backgroundColor = UIColor.white
+
+        // デリゲートを設定
+        weightInputField.delegate = self
+        heightInputField.delegate = self
         
+        // クリアボタンの設定
+        weightInputField.clearButtonMode = .whileEditing
+        heightInputField.clearButtonMode = .whileEditing
         
-        // 体重入力欄の作成
+        // 体重入力欄の作成 ---------------------------------------------------------------
+        
+        // 体重入力タイトル表示
         let weightLabel = UILabel()
         weightLabel.textColor = UIColor(named: "textGray") // ボタンの色をグレー色（textGray）に設定
         weightLabel.font = UIFont.systemFont(ofSize: 20) // フォントサイズを変更
-        weightLabel.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.4,
+        weightLabel.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.2,
                                   width: view.frame.width * 0.8, height: view.frame.height * 0.1) // 表示位置を設定
         weightLabel.textAlignment = NSTextAlignment.left // 左寄せ
         weightLabel.text = "体重を入力" // タイトルを設定
         self.view.addSubview(weightLabel)
+
+        // 体重入力の単位表示
+        let weightUnitLabel = UILabel()
+        weightUnitLabel.textColor = UIColor(named:"textGray") // ボタンの色をグレー色（textGray）に設定
+        weightUnitLabel.font = UIFont.systemFont(ofSize: 20) // フォントサイズを変更
+        weightUnitLabel.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.25,
+                                   width: view.frame.width * 0.8, height: view.frame.height * 0.1) // 表示位置を設定
+        weightUnitLabel.textAlignment = NSTextAlignment.right // 右寄せ
+        weightUnitLabel.text = "Kg"
+        self.view.addSubview(weightUnitLabel)
         
-//        let weightInputField = UITextField()
-        weightInputField.keyboardType = UIKeyboardType.default
-//        weightInputField.keyboardType = UIKeyboardType.decimalPad
-//        weightInputField.returnKeyType = UIReturnKeyType.default
-        weightInputField.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.5,
-                                        width: view.frame.width * 0.8, height: view.frame.height * 0.1) // 表示位置を設定
+        // 体重入力欄の作成
+        weightInputField.keyboardType = UIKeyboardType.default // キーボードのタイプを設定　※デフォルト
+        weightInputField.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.25,
+                                        width: view.frame.width * 0.7, height: view.frame.height * 0.1) // 表示位置を設定
         weightInputField.textAlignment = NSTextAlignment.left // 左寄せ
+        weightInputField.placeholder = "体重を入力" // 未入力の場合の表示文字を設定
         self.view.addSubview(weightInputField)
-        
-        weightInputField.placeholder = "体重を入力" // 未入力の場合に文字を表示
+
+        // 下線を表示（体重入力欄）
+        let underLineOfweight =  UIView()
+        underLineOfweight.backgroundColor = UIColor(named: "textGray") // ボタンの色をグレー色（textGray）に設定
+        underLineOfweight.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.32,
+                                 width: view.frame.width * 0.8, height: 1.0) // 表示位置を設定
+        self.view.addSubview(underLineOfweight)
+
         
         // returnkeyが押された後の処理が必要。
         // 入力した結果はvalueに格納される。
-       
-        let underLine =  UIView()
-        underLine.backgroundColor = UIColor.black
-        underLine.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.6,
-                                 width: view.frame.width * 0.8, height: 1.0) // 表示位置を設定
-        self.view.addSubview(underLine)
-        
-        /*
-        let border = CALayer()
-        var color = UIColor()
-        border.backgroundColor = color.cgColor
-        weightInputField.layer.addSublayer(border)
-        */
 
-        // 身長入力欄の作成
+        
+        // 身長入力欄の作成 ---------------------------------------------------------------
+
+        // 身長入力タイトル表示
         let heightLabel = UILabel()
         heightLabel.textColor = UIColor(named: "textGray") // ボタンの色をグレー色（textGray）に設定
         heightLabel.font = UIFont.systemFont(ofSize: 20) // フォントサイズを変更
-        heightLabel.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.6,
+        heightLabel.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.35,
                                    width: view.frame.width * 0.8, height: view.frame.height * 0.1) // 表示位置を設定
         heightLabel.textAlignment = NSTextAlignment.left // 左寄せ
         heightLabel.text = "身長を入力" // タイトルを設定
         self.view.addSubview(heightLabel)
 
-        // BMI計算ボタンの生成
+        // 身長入力の単位表示
+        let heightUnitLabel = UILabel()
+        heightUnitLabel.textColor = UIColor(named:"textGray") // ボタンの色をグレー色（textGray）に設定
+        heightUnitLabel.font = UIFont.systemFont(ofSize: 20) // フォントサイズを変更
+        heightUnitLabel.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.4,
+                                       width: view.frame.width * 0.8, height: view.frame.height * 0.1) // 表示位置を設定
+        heightUnitLabel.textAlignment = NSTextAlignment.right // 右寄せ
+        heightUnitLabel.text = "cm"
+        self.view.addSubview(heightUnitLabel)
+        
+        // 身長入力欄の作成
+        heightInputField.keyboardType = UIKeyboardType.default // キーボードのタイプを設定　※デフォルト
+        heightInputField.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.4,
+                                        width: view.frame.width * 0.7, height: view.frame.height * 0.1) // 表示位置を設定
+        heightInputField.textAlignment = NSTextAlignment.left // 左寄せ
+        heightInputField.placeholder = "身長を入力" // 未入力の場合の表示文字を設定
+        self.view.addSubview(heightInputField)
+        
+        // 下線を表示（身長入力欄）
+        let underLineOfheight =  UIView()
+        underLineOfheight.backgroundColor = UIColor(named: "textGray") // ボタンの色をグレー色（textGray）に設定
+        underLineOfheight.frame = CGRect(x: view.frame.width * 0.1, y: view.frame.height * 0.47,
+                                         width: view.frame.width * 0.8, height: 1.0) // 表示位置を設定
+        self.view.addSubview(underLineOfheight)
+
+        
+        
+        
+        
+        // BMI計算ボタンの生成 ---------------------------------------------------------------
+        
         let bmiCalcButton = UIButton()
         bmiCalcButton.backgroundColor = UIColor(named: "buttonPink")   // ボタンの色をピンク色（buttonPink）に設定
         bmiCalcButton.frame = CGRect(x: view.frame.width * 0.25, y: view.frame.height * 0.8,
@@ -86,8 +131,7 @@ class InputViewController: UIViewController, UITextFieldDelegate {
     // BMI計算ボタンを押された時の処理
     @objc func bmiCalcButtonClicked(sender: UIButton){
         print("bmiCalcButtonClicked")
-        let resultVC:ResultViewController = ResultViewController()
-//        self.navigationController?.pushViewController(resultVC, animated: true)
+        let resultVC: ResultViewController = ResultViewController()
         resultVC.str = weightInputField.text!
         self.present(resultVC, animated: true) // 下からニュッと出る。モーダル遷移。
     }
@@ -101,5 +145,13 @@ class InputViewController: UIViewController, UITextFieldDelegate {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // 改行ボタンを押された場合の処理
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("wValue:\(Int(weightInputField.text!)!)")
+        print("hValue:\(Int(heightInputField.text!)!)")
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
